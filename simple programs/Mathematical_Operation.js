847cbf7f6985c438a1033d0b49c26308109e9549
k-sharmila <sharmilakaruppanan3@gmail.com>
2022-10-18 05:25:50 -0700

    program executed

---
+++ simple programs/Mathematical_Operation.js
@@ -0,0 +1,13 @@
+//Addition , Subtraction,Multiplication,Division,Modulo of Two Numbers
+
+let a=3,b=4,sum,sub,mul,div,mod;
+sum=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+console.log("The Sum is :",sum);
+console.log("The Subtraction is :",sub);
+console.log("The Multiplication is :",mul);
+console.log("The Division is :",div);
+console.log("The Modulo is :",mod);
