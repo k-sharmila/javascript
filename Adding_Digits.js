228570a25e6cba116f51e5dd0d4dfb240a618aad
Sharmila <96688536+k-sharmila@users.noreply.github.com>
2022-11-01 06:44:35 -0700

    Delete Adding_Digits.js

--- Adding_Digits.js
+++
@@ -1,9 +0,0 @@
-//Adding Digits of a number
-var a=4567899235,sum=0,mod;
-while(a)
-{
-mod=a%10;
-sum=sum+mod;
-a= Math.floor(a/10);  //Math.floor rounds of your answer to its nearest integer
-}
-console.log("The Sum of digit is :",sum);
