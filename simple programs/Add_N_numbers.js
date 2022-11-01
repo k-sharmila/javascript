0505dffc87fbf882500414f13f25e4f9eaa692f2
Sharmila <96688536+k-sharmila@users.noreply.github.com>
2022-11-01 06:43:27 -0700

    Add files via upload

---
+++ simple programs/Add_N_numbers.js
@@ -0,0 +1,8 @@
+//Adding N numbers in an array
+var i,n=9,sum=0;
+array=new Array(10,2,23,45,32,677,45,90,55);
+for(i=0;i<9;i++)
+{
+    sum=sum+array[i];
+}
+console.log("The sum of an array is :",sum);
---
+++ simple programs/Factorial.js
@@ -0,0 +1,7 @@
+//Factorial of a Number
+var fact=1,i,n=30;
+for(i=1;i<=30;i++)
+{
+  fact=fact*i;
+}
+console.log("Fact is :",fact);
