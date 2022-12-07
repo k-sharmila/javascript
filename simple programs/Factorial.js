21967a6c3aa581630a33830118959238bdc69ad2
Sharmila <96688536+k-sharmila@users.noreply.github.com>
2022-12-07 01:54:14 -0800

    input by user

--- simple programs/Factorial.js
+++ simple programs/Factorial.js
@@ -1,7 +1,21 @@
-//Factorial of a Number
-var fact=1,i,n=30;
-for(i=1;i<=30;i++)
-{
-  fact=fact*i;
-}
-console.log("Fact is :",fact);
+<!--Factorial of a Number-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Factorial</title>
+  </head>
+  <body></body>
+  <script>
+    let fact = 1,
+      n,
+      i;
+    n = parseInt(prompt("Enter n"));
+    for (i = 1; i <= n; i++) {
+      fact = fact * i;
+    }
+    document.write("The factorial of is:", fact);
+  </script>
+</html>
